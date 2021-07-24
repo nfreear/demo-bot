8055bcda4afba7518f7fe94e70c78a0c27aa372b
Nick Freear <nfreear@yahoo.co.uk>
2021-07-24 10:34:53 +0100

    feat: Continue with `survey-bot` integration; Analytics (#12) (#11)
    * Plus fix `NlpWeb.processActivity()` method

--- src/getNlpConfigSurvey.ts
+++ src/getNlpConfigSurvey.ts
@@ -1,12 +1,12 @@
 /**
- * Configuration for a `Survey-Bot`.
+ * NLP.js configuration for a `Survey-Bot`.
  */
 
 import { Nlp } from '@nlpjs/nlp';
 import { LangEn } from '@nlpjs/lang-en-min';
 
 import { Configuration } from './nlpjsTypes';
-import { SurveyBot } from '../survey-bot/index.js';
+import { SurveyBot } from './surveyBotApp';
 
 export function getNlpConfig(): Configuration {
     return {
